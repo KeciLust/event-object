7ad10694013f37ec17563fe400b761c076a0b4ad
Denis Nikitin <dinya.nikitin@gmail.com>
2021-02-28 15:12:10 +0300

    1 Finish

---
+++ dropdown/task.js
@@ -0,0 +1,16 @@
+const dropList = Array.from(document.querySelectorAll(`.dropdown__list`));
+const dropdown = Array.from(document.querySelectorAll(`.dropdown__value`));
+const dropItem = Array.from(document.querySelectorAll(`.dropdown__link`))
+dropdown.forEach(el => {
+    el.addEventListener(`click`, () => {
+        dropList[0].classList.toggle(`dropdown__list_active`);
+    })
+})
+dropItem.forEach(el => {
+    el.onclick = () => {
+        dropdown[0].textContent = el.textContent;
+        dropList[0].classList.remove(`dropdown__list_active`);
+        return false;
+    }
+     
+})
